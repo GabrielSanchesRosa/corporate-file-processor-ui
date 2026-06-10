959880b8de422cd6cf29c1bb677f86da06f595c4
Gabriel Sanches <gabrielsanches131202@gmail.com>
2026-06-10 18:51:55 -0300

    feat: phase 4 - dashboard stats cards

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -1,4 +1,6 @@
+import { Grid } from "@mui/material"
 import { useDashboardStats } from "./useDashboardStats"
+import StatCard from "../../shared/components/StatCard"
 
 function DashboardPage() {
     const { data, isLoading, isError } = useDashboardStats()
@@ -7,13 +9,20 @@ function DashboardPage() {
     if (isError) return <p>Error loading data.</p>
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Total files: {data?.totalFiles}</p>
-            <p>Total errors: {data?.totalErrors}</p>
-            <p>Total records: {data?.totalRecordsProcessed}</p>
-            <p>Average time: {data?.averageProcessingTimeInSeconds}</p>
-        </div>
+        <Grid container spacing={3}>
+            <Grid size={3}>
+                <StatCard title="Total files" value={data?.totalFiles ?? 0}></StatCard>
+            </Grid>
+            <Grid size={3}>
+                <StatCard title="Total errors" value={data?.totalErrors ?? 0}></StatCard>
+            </Grid>
+            <Grid size={3}>
+                <StatCard title="Total records" value={data?.totalRecordsProcessed ?? 0}></StatCard>
+            </Grid>
+            <Grid size={3}>
+                <StatCard title="Average time" value={`${(data?.averageProcessingTimeInSeconds ?? 0).toFixed(2)}s`}></StatCard>
+            </Grid>
+        </Grid>
     )
 }
 
